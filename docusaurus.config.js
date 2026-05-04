ffe1df167e77fc497bfae999290eb52ac254e54b
Sulagna Sasmal(UST <309676@ust.com>
2026-05-05 00:06:10 +0530

    Add local search plugin (docusaurus-search-local)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,6 +48,19 @@ const config = {
     ],
   ],
 
+  themes: [
+    [
+      '@easyops-cn/docusaurus-search-local',
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        hashed: true,
+        docsRouteBasePath: '/docs',
+        indexBlog: false,
+        highlightSearchTermsOnTargetPage: true,
+      }),
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
